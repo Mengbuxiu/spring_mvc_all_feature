845da4ba47b7d01b7a20a14300025bb22a6d15d7
lin <woooodlin@gmail.com>
2020-06-19 15:28:50 +0800

    RequestHeader

--- src/main/java/com/oukele/spring/controller/LoginController.java
+++ src/main/java/com/oukele/spring/controller/LoginController.java
@@ -1,12 +1,14 @@
 package com.oukele.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
     @RequestMapping("/login")
-    public String login() {
+    public String login(@RequestHeader(value = "Cache-Control", required = false) String header) {
+        System.out.println("header = " + header);
         return "index";
     }
 
