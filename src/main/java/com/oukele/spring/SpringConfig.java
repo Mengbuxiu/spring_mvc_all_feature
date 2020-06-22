56bac961e69fcb8425b58123b9229489ff30ce04
lin <woooodlin@gmail.com>
2020-06-22 16:49:54 +0800

    消息转换器

--- src/main/java/com/oukele/spring/SpringConfig.java
+++ src/main/java/com/oukele/spring/SpringConfig.java
@@ -1,34 +1,91 @@
 package com.oukele.spring;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration//声明当前类是一个配置类（类似于 spring_xxx.xml 的文件）
 @EnableWebMvc//若无此注解，WebMvcConfigurerAdapter无效
 @ComponentScan//自动扫描spring注解 比如@Service、@Component、@Repository和@Controller的类，并注册为Bean
 public class SpringConfig extends WebMvcConfigurerAdapter {
 
-    //添加一个ViewResolver解析view
-    @Bean//相当于Spring配置文件bean节点
-    public InternalResourceViewResolver viewResolver(){
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //创建fastjson消息转换器
+        FastJsonHttpMessageConverter fastJsonConverter = new FastJsonHttpMessageConverter();
+        List<MediaType> supportedMediaTypes = new ArrayList<>();
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        supportedMediaTypes.add(MediaType.APPLICATION_ATOM_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+        supportedMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+        supportedMediaTypes.add(MediaType.APPLICATION_PDF);
+        supportedMediaTypes.add(MediaType.APPLICATION_RSS_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_XHTML_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_XML);
+        supportedMediaTypes.add(MediaType.IMAGE_GIF);
+        supportedMediaTypes.add(MediaType.IMAGE_JPEG);
+        supportedMediaTypes.add(MediaType.IMAGE_PNG);
+        supportedMediaTypes.add(MediaType.TEXT_EVENT_STREAM);
+        supportedMediaTypes.add(MediaType.TEXT_HTML);
+        supportedMediaTypes.add(MediaType.TEXT_MARKDOWN);
+        supportedMediaTypes.add(MediaType.TEXT_PLAIN);
+        supportedMediaTypes.add(MediaType.TEXT_XML);
+        // 强制手动设置
+        fastJsonConverter.setSupportedMediaTypes(supportedMediaTypes);
+        // 创建配置类
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        // 修改配置返回内容的过滤
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat, SerializerFeature.DisableCircularReferenceDetect, SerializerFeature.WriteMapNullValue,
+                SerializerFeature.WriteNullStringAsEmpty, SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteNullNumberAsZero, SerializerFeature.WriteNullBooleanAsFalse);
+
+        fastJsonConverter.setFastJsonConfig(fastJsonConfig);
+
+        // 添加StringHttpMessageConverter，解决中文乱码问题
+        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
+        List<MediaType> mediaTypes = Collections.singletonList(MediaType.APPLICATION_JSON_UTF8);
+        stringConverter.setSupportedMediaTypes(mediaTypes);
+
+        // fastJson添加到消息转换列表
+        converters.add(fastJsonConverter);
+        converters.add(stringConverter);
+    }
+
+    // 添加一个ViewResolver解析view
+    @Bean// 相当于Spring配置文件bean节点
+    public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/views/");
         viewResolver.setSuffix(".jsp");
         viewResolver.setViewClass(JstlView.class);
         return viewResolver;
     }
-    //注册静态资源，没注册的话，网站是访问不了的
+
+    // 注册静态资源，没注册的话，网站是访问不了的
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/js/");
         registry.addResourceHandler("/fonts/**").addResourceLocations("/WEB-INF/fonts/");
     }
-    //根目录的时候直接跳转到登录界面
+
+    // 根目录的时候直接跳转到登录界面
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("Login");
--- src/main/java/com/oukele/spring/controller/LoginController.java
+++ src/main/java/com/oukele/spring/controller/LoginController.java
@@ -1,8 +1,15 @@
 package com.oukele.spring.controller;
 
+import com.oukele.spring.entity.Person;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class LoginController {
@@ -10,6 +17,16 @@ public class LoginController {
     public String login(@RequestHeader(value = "Cache-Control", required = false) String header) {
         System.out.println("header = " + header);
         return "index";
+    }
+
+    @GetMapping("/person")
+    public ResponseEntity<Person> showBook() {
+        Person person = new Person("精神小伙", 100);
+        return ResponseEntity
+                .ok()
+                .cacheControl(CacheControl.maxAge(30, TimeUnit.DAYS))
+                .eTag("1.1.1") // lastModified is also available
+                .body(person);
     }
 
 }
